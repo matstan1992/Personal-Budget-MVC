4547eaee8f847e800ac92e08c3758427e6290392
Mateusz Stankowski <stankowski1992@gmail.com>
2021-04-12 18:54:33 +0200

    Added functions for remembering the logged in user and logging out (without project page)

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -33,6 +33,10 @@ class Login extends \Core\Controller
         $user = User::authenticate($_POST['email'], $_POST['password']);
 
         if ($user) {
+			
+			session_regenerate_id(true);
+
+            $_SESSION['user_id'] = $user->id;
 
             $this->redirect('/');
 
@@ -42,5 +46,36 @@ class Login extends \Core\Controller
                 'email' => $_POST['email'],
             ]);
         }
+    }
+	
+	/**
+     * Log out a user
+     *
+     * @return void
+     */
+    public function destroyAction()
+    {
+        // Unset all of the session variables
+        $_SESSION = [];
+
+        // Delete the session cookie
+        if (ini_get('session.use_cookies')) {
+            $params = session_get_cookie_params();
+
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params['path'],
+                $params['domain'],
+                $params['secure'],
+                $params['httponly']
+            );
+        }
+
+        // Finally destroy the session
+        session_destroy();
+
+        $this->redirect('/');
     }
 }
